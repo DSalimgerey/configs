31a6fad00db7db6a9ab368a9d46e3ca9bd4546c8
DSalimgerey <didarsalimgerey@gmail.com>
2024-03-10 15:21:47 +0500

    set cursor type block on insert mode

--- init.lua
+++ init.lua
@@ -30,6 +30,8 @@ vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
+vim.opt.guicursor = true
+
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
